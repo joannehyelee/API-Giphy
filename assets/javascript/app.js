042b28d02a6d50ea5d2e4be7d92e43ad1d334bcd
Joanne Kim <joannehyelee@gmail.com>
2018-03-29 15:15:52 -0700

    Display images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,12 +16,16 @@ $(document).ready(function(){
     }).then(function(response){
 
         var results = response.data;
-        // console.log(results);
+        console.log(results);
 
         for (var i = 0; i < results.length; i++) {
-            console.log(results);
-            console.log(results[i].rating);
-            console.log(results[i].images.fixed_height);
+            var imagesDiv = $('<div class="col-md-6">');
+            var image = $('<img src="' + results[i].images.fixed_height.url + '">');
+            var ratingP = $('<p>Rating: ' + results[i].rating.toUpperCase() + '</p>');
+            imagesDiv.append(image);
+            imagesDiv.append(ratingP);
+            $("#images-div").append(imagesDiv);
         }
+
     });
 });
